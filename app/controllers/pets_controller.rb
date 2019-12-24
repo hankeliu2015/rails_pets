70392e1b191d36f3064150bfb02d51f5515e4d4e
github username <github email address>
2019-12-24 15:42:50 -0500

    add validation conditin for create action.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,5 +5,21 @@ class PetsController < ApplicationController
   end
 
   def create
-  end 
+    @pet = Pet.create(pet_params)
+    binding.pry
+    # byebug
+    # @pet = Pet.create(name: params[:dog][:name])
+
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+       render :new
+    end
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :species, :age)
+  end
 end
